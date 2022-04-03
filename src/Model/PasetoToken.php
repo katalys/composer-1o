0b6355a991665bbaf1ac5c220d709dfcef1f7bb8
Marko Ambrožič <marko@webam.si>
2022-04-03 10:21:09 +0200

    Added verification logic

--- src/Model/PasetoToken.php
+++ src/Model/PasetoToken.php
@@ -9,7 +9,7 @@ class PasetoToken
     public function getSignedToken($sharedKey, string $footer = '', string $exp = 'P01D')
     {
         if (!$this->token) {
-            $sharedKey = new \ParagonIE\Paseto\Keys\SymmetricKey($sharedKey);
+            $sharedKey = new \ParagonIE\Paseto\Keys\SymmetricKey(base64_decode($sharedKey));
             $token = (new \ParagonIE\Paseto\Builder())
                 ->setKey($sharedKey)
                 ->setVersion(new \ParagonIE\Paseto\Protocol\Version2)
@@ -23,5 +23,25 @@ class PasetoToken
         }
 
         return $this->token;
+    }
+    
+    public function verifyToken($token, $sharedKey, $footer)
+    {
+        $key = new \ParagonIE\Paseto\Keys\SymmetricKey(base64_decode($sharedKey));
+        $decryptedToken = \ParagonIE\Paseto\Protocol\Version2::decrypt($token, $key, $footer);
+        $rawDecryptedToken = json_decode($decryptedToken);
+        if (is_object($rawDecryptedToken) && isset($rawDecryptedToken->exp)) {
+            $checkTime = new \DateTime('NOW');
+            $checkTime = $checkTime->format(\DateTime::ATOM);
+            $tokenExp = new \DateTime($rawDecryptedToken->exp);
+            $tokenExp = $tokenExp->format(\DateTime::ATOM);
+            if ($checkTime > $tokenExp) {
+                return true; // expired - do nothing else!!
+            } else {
+                return false; // trust token - not expired and has valid signature.
+            }
+        } else {
+            return true;
+        }
     }
 }
